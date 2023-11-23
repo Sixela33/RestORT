d51d20d83069cca3c7821de6a3daeea40e503fef
marvangonzalez <82179028+marvangonzalez@users.noreply.github.com>
2023-11-23 16:27:55 -0300

    obrigado

--- testDeInsumos/api-ini.test.js
+++ testDeInsumos/api-ini.test.js
@@ -1,46 +1,52 @@
 import { expect } from "chai"
 import supertest from "supertest"
-import generador from './generador/insumo.js'
-import ExpressServer from "../server.js"
+import Server from "../server.js"
+
+
+const request = supertest('http://127.0.0.1:8080')
 
 describe('test apirestful', () => {
     describe('GET', () => {
-        it('debería retornar un status 200', async () => {
-            const server = new ExpressServer(8081, process.env.DATABASE)
-            const app = await server.start()
+        it('debería retornar un insumo', async () => {
 
+            const server = new Server(8081, process.env.DATABASE)
+            const app = await server.start()
             const request = supertest(app)
-            const response = await request.get('/api/insumos')
-            
+
+            const res = await request.post('/api/users/login').send({ documento: "00000000000", contrasena: "qwerqwer" })
+
+            const response = await request.get('/api/insumos/1').set({"Authorization": res.body.token})
+  
             expect(response.status).to.eql(200)
+            const insumo_buscado = response.body[0]
+
+            expect(insumo_buscado).to.include.keys('insumoid','nombre','cantidad','costoxunidad','unidaddemedida')
+
+            expect(insumo_buscado.insumoid).to.eql(1)
+            expect(insumo_buscado.nombre).to.eql('Harina')
+            expect(insumo_buscado.cantidad).to.eql("-89")
+            expect(insumo_buscado.costoxunidad).to.eql('100')
+            expect(insumo_buscado.unidaddemedida).to.eql('KG')
 
             await server.stop()
         })
     })
 
-    describe('POST', () => {
-        it('debería incorporar un insumo', async () => {
-            const server = new ExpressServer(8081,process.env.DATABASE)
-            const app = await server.start()
+    describe('GET', () => {
+        it('debería retornar todos los insumos', async () => {
 
+            const server = new Server(8081, process.env.DATABASE)
+            const app = await server.start()
             const request = supertest(app)
 
-            const insumo = generador.get()
-            console.log(insumo)
+            const res = await request.post('/api/users/login').send({ documento: "00000000000", contrasena: "qwerqwer" })
 
-            const response = await request.post('/api/insumos').send(insumo)
+            const response = await request.get('/api/insumos').set({"Authorization": res.body.token})
+
             expect(response.status).to.eql(200)
 
-            const insumoGuardado = response.body
-            //console.log(insumoGuardado)
-            expect(insumoGuardado).to.include.keys('nombre','cantidad','costoXunidad','unidadDeMedida')
-
-            expect(insumoGuardado.nombre).to.eql(insumo.nombre)
-            expect(insumoGuardado.cantidad).to.eql(insumo.cantidad)
-            expect(insumoGuardado.costoXunidad).to.eql(insumo.costoXunidad)
-            expect(insumoGuardado.unidadDeMedida).to.eql(insumo.unidadDeMedida)
-
             await server.stop()
+
         })
     })
 })
